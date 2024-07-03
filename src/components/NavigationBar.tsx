61ee215314c5a159a833af130f4c628009d641a9
junseooo <pointjunseo@gmail.com>
2024-07-03 17:51:36 +0900

    header notice svg image 추가

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -12,7 +12,21 @@ export default function NavigationBar() {
                 연차 촉진 제도, 100% 자동화하기
               </span>
             </div>
-            <div>image</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="flex-shrink:0"
+            >
+              <path
+                fill="#556372"
+                d="M9.783 17.566 8.65 16.434 13.086 12 8.65 7.566l1.132-1.132L15.349 12l-5.566 5.566Z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </div>
         </a>
       </div>
